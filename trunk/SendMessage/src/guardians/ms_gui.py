05d782c9e412f2d6804d3d6b7dc217064f29aba8
miis.araujo <miis.araujo@b70c22c8-a34a-0410-957e-83a1bcb8dcd7>
2008-04-18 23:13:30 +0000

    Initial import.

---
+++ trunk/SendMessage/src/guardians/ms_gui.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+
+import sys
+try:
+     import pygtk
+     pygtk.require("2.0")
+except:
+      pass
+try:
+    import gtk
+    import gtk.glade
+except:
+    sys.exit(1)
+    
+class SendMessageGUI:
+    """This is a GUI for the SendMessage application"""
+
+    def __init__(self):
+                
+        #Set the Glade file
+        self.gladefile = "sMessage2.glade"  
+        self.wTree = gtk.glade.XML(self.gladefile) 
+                 
+        #Create our dictionay and connect it
+        dic = { "on_cancel_clicked" : self.cancel_clicked,
+                "on_sendMessage_clicked" : self.sendMessage_clicked,
+                "on_dialog1_destroy" : gtk.main_quit }
+        self.wTree.signal_autoconnect(dic)
+        
+    def cancel_clicked(self, widget):
+           print "Hello World!"
+    
+    def sendMessage_clicked(self, widget):
+           print "Hello World!"
+            
+if __name__ == "__main__":
+    smg = SendMessageGUI()
+    gtk.main()
