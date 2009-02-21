18c34d870523927468a068891edeb1a52603fc66
matheusgr <matheusgr@b70c22c8-a34a-0410-957e-83a1bcb8dcd7>
2009-02-21 19:46:09 +0000

    Created TestClass.

--- trunk/gat/src/guardians/qc_model.py
+++ trunk/gat/src/guardians/qc_model.py
@@ -14,7 +14,7 @@ class QuotaCheckModel:
         self.uid = os.getuid()
         self.hosts = [('coisafofa','/home'), ('seulunga','/home')]
     
-    def __getList(self, host, disk, status, value):
+    def _getList(self, host, disk, status, value):
         value_list = list(value)
         value_list.insert(0, host)
         value_list.insert(1, disk)
@@ -27,12 +27,25 @@ class QuotaCheckModel:
             try:
                 (status, value) = rquota.getQuotaTCP(host, disk, self.uid)
                 if status == STATUS_OK:
-                    result.append(self.__getList(host,disk,status,value))
+                    result.append(self._getList(host,disk,status,value))
                 else:
-                    result.append(self.__getList(host,disk,status,()))
+                    result.append(self._getList(host,disk,status,()))
             except:
-                result.append(self.__getList(host,disk,STATUS_EXCEPT,()))
+                result.append(self._getList(host,disk,STATUS_EXCEPT,()))
         return result
+
+class QuotaCheckModelTest(QuotaCheckModel):
+    def __init__(self):
+        self.uid = os.getuid()
+
+    def getQuota(self):
+        result = []
+        result.append(self._getList('host1', 'home', STATUS_OK, range(7,1,-1)))
+        result.append(self._getList('host1', 'tmp', STATUS_OK,  range(7,1,-1)))
+        result.append(self._getList('host2', 'bck', STATUS_OK,  range(7,1,-1)))
+        result.append(self._getList('host3', 'bck', STATUS_NOQUOTA,()))
+        return result
+        
             
 def main():
     print QuotaCheckModel().getQuota()
