6603301849f43d256fc6106482ced4fa0cdc72c7
matheusgr <matheusgr@b70c22c8-a34a-0410-957e-83a1bcb8dcd7>
2009-02-21 18:43:06 +0000

    Using relative path instead of full path.

--- gat/src/guardians/dc_model.py
+++ gat/src/guardians/dc_model.py
@@ -1,23 +1,33 @@
 import os
+import os.path
 from tarfile import TarFile
 
-def _recursive_list(directory, result):
-    try:
-        listdir = os.listdir(directory)
-    except (OSError):
-        size = os.lstat(directory).st_size
-        result.append((directory, size))
+def _recursive_list(base_dir, current, result):
+    """
+    List a directory recursively. Result list will return a tree where each
+    node represents a directory or a file. When a node is a file, it will
+    be a list of two parameters: file name and file size. When a node is a
+    directory, it will contains also a list of nodes of given directory.
+    """
+    ffile = base_dir + os.sep + current
+    if os.path.isdir(ffile):
+        listdir = os.listdir(ffile)
+    elif os.path.isfile(ffile):
+        size = os.lstat(ffile).st_size
+        result.append([current, size])
         return size
     cur_result = []
     size = 0
-    for dir in listdir:
-        size += _recursive_list(directory + os.sep + dir, cur_result)
-    result.append((directory, cur_result, size))
+    for d in listdir:
+        size += _recursive_list(ffile, d, cur_result)
+    cur_result.sort(lambda x, y: cmp(x[-1], y[-1])) # Sort by size
+    result.append([current, cur_result, size])
     return size
 
 def get_list(directory):
     result = []
-    _recursive_list(directory, result)
+    _recursive_list(directory, '.', result)
+    result[0][0] = directory
     return result
 
 def recursive_delete(directory):
