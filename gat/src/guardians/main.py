10f75d9a196d3f7eebcfcbef61cba6cc5faca90b
matheusgr <matheusgr@b70c22c8-a34a-0410-957e-83a1bcb8dcd7>
2009-03-11 08:13:06 +0000

    Connected buttons to functions.

--- gat/src/guardians/main.py
+++ gat/src/guardians/main.py
@@ -2,7 +2,7 @@ import functools
 
 from PyQt4 import QtCore, QtGui
 
-import disk, quota
+import disk, quota, mail
 import gui
 import config
 
@@ -91,7 +91,8 @@ model_update.set_ui()
 
 message_widget = QtGui.QFrame(main_widget)
 message_widget.hide()
-gui.message.Ui_SendMailWidget().setupUi(message_widget)
+message_frame = gui.message.Ui_SendMailWidget()
+message_frame.setupUi(message_widget)
 
 faq_widget = QtGui.QFrame(main_widget)
 faq_widget.hide()
@@ -104,23 +105,39 @@ QtCore.QObject.connect(form.quota_button, QtCore.SIGNAL("clicked()"), functools.
 QtCore.QObject.connect(form.message_button, QtCore.SIGNAL("clicked()"), functools.partial(lambda x : new_widget(x), message_widget))
 QtCore.QObject.connect(form.faq_button, QtCore.SIGNAL("clicked()"), functools.partial(lambda x : new_widget(x), faq_widget))
 
-def test():
-    """ TODO: Remove. Dont use in final version.... """
-    import os.path
-    dirs = set([ x.internalPointer() for x in quota_frame.diskTreeView.selectedIndexes() ])
-    for dir in dirs:
-        path = ''
-        i_tmp = dir
-        while i_tmp.parent:
-            if i_tmp.parent.is_root():
-                path = i_tmp.name + path
-            else:
-                path = os.path.sep + i_tmp.name + path
-            i_tmp = i_tmp.parent
-        #print path
+def get_selection():
+    return quota_frame.diskTreeView.selectedIndexes()[0].internalPointer()
+
+def delete():
+    src = get_selection()
+    disk.recursive_delete(src)
     model_update.update()
 
-QtCore.QObject.connect(quota_frame.deleteButton, QtCore.SIGNAL("clicked()"), test)
+def move():
+    src = get_selection()
+    dest_dir = QtGui.QFileDialog.getExistingDirectory()
+    disk.move(src, dest_dir)
+    model_update.update()
+
+def compact():
+    src = get_selection()
+    dest_filename = QtGui.QFileDialog.getSaveFileName()
+    disk.compact(src, dest_filename)
+    model_update.update()
+
+def send_mail():
+    import os
+    uid = unicode(os.getuid())
+    subject = unicode(message_frame.lineEdit.text())
+    message = unicode(message_frame.plainTextEdit.document().toPlainText())
+    body = '\n' + 'uid - ' + uid + '\n' + 'subject - ' + subject + '\n' + message
+    mail.MailToGuardians('gat_tool@lcc.ufcg.edu.br', body).send_mail()
+
+QtCore.QObject.connect(quota_frame.deleteButton, QtCore.SIGNAL("clicked()"), delete)
+QtCore.QObject.connect(quota_frame.moveButton, QtCore.SIGNAL("clicked()"), move)
+QtCore.QObject.connect(quota_frame.compactButton, QtCore.SIGNAL("clicked()"), compact)
+
+QtCore.QObject.connect(message_frame.pushButton, QtCore.SIGNAL("clicked()"), send_mail)
 
 main_widget.show()
 app.setStyle('cleanlooks')
